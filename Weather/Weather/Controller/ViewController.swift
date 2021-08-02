aa2433475381e23a1b6856233b32f3f1e2e89bbe
Admalll <F1zzer1997195@gmail.com>
2021-08-02 19:58:16 +0300

    Добалено: - скролл вью - логин - пароль - кнопка входа
    Также добавлены констрейнты для всех элементов

--- Weather/Weather/Controller/ViewController.swift
+++ Weather/Weather/Controller/ViewController.swift
@@ -9,8 +9,67 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
+    //MARK: - IBOutlets
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var mainScrollView: UIScrollView!
+
+    //MARK: - UIViewController
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name:
+                                                    UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:
+                                                    UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    //MARK: - Private methods
+
+    private func setupView() {
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        mainScrollView.addGestureRecognizer(hideKeyboardGesture)
+    }
+
+    @objc private func keyboardWillShow(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let keyboardSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+
+        mainScrollView.contentInset = contentInsets
+        mainScrollView.scrollIndicatorInsets = contentInsets
+    }
+
+    @objc private func keyboardWillHide(notification: Notification) {
+        mainScrollView.contentInset = UIEdgeInsets.zero
+    }
+
+    @objc private func hideKeyboard() {
+        mainScrollView.endEditing(true)
+    }
+
+    //MARK: - IBActions
+
+    @IBAction private func loginButtonTapped(_ sender: UIButton) {
+        guard let login = loginTextField.text, let password = passwordTextField.text else { return }
+        login == "admin" && password == "123456" ? print("успешная авторизация") : print("неуспешная авторизация")
     }
 }
 
