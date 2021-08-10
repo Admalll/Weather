3cf0f7d37785d62e5968a2dc0a36b661c3080fcb
Admalll <F1zzer1997195@gmail.com>
2021-08-10 17:42:02 +0300

    Урок 7

--- Weather/Weather/Controller/LoginViewController.swift
+++ Weather/Weather/Controller/LoginViewController.swift
@@ -11,6 +11,7 @@ final class LoginViewController: UIViewController {
 
     //MARK: - IBOutlets
 
+    @IBOutlet private weak var loginLabel: UILabel!
     @IBOutlet private weak var loginTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var mainScrollView: UIScrollView!
@@ -27,6 +28,8 @@ final class LoginViewController: UIViewController {
         super.viewWillAppear(animated)
 
         setupNotifications()
+        animateLoginLabel()
+        animateLoginField()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -42,12 +45,44 @@ final class LoginViewController: UIViewController {
 
     //MARK: - Private methods
 
+    private func animateLoginField() {
+        self.loginTextField.transform = CGAffineTransform(translationX: -500, y: 0)
+        UIView.animate(withDuration: 1.0) {
+            self.loginTextField.transform = .identity
+        }
+    }
+
+    private func animateLoginLabel() {
+        let fadeInAnimation = CABasicAnimation(keyPath: "opacity")
+        fadeInAnimation.fromValue = 0
+        fadeInAnimation.toValue = 1
+        fadeInAnimation.beginTime = CACurrentMediaTime() + 1
+        fadeInAnimation.fillMode = .backwards
+        fadeInAnimation.duration = 1
+        loginLabel.layer.add(fadeInAnimation, forKey: nil)
+
+    }
+
+    private func animateWrongPassword() {
+        UIView.animate(withDuration: 0.6, delay: 0.0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.1, options: [.autoreverse]) {
+            self.passwordTextField.frame.origin.x += 1
+            self.passwordTextField.frame.origin.y += 1
+        } completion: { (result) in
+            self.passwordTextField.layer.borderColor = UIColor.red.cgColor
+            self.passwordTextField.layer.borderWidth = 1.0
+            self.passwordTextField.frame.origin.x -= 1
+            self.passwordTextField.frame.origin.y -= 1
+        }
+
+    }
+
     private func checkLoginInfo() -> Bool {
         guard let login = loginTextField.text, let password = passwordTextField.text else { return false }
         if login == "admin" && password == "123456" {
             return true
         } else {
-            showErrorAlert(alertText: "Ошибка", alertMessage: "Пользователь не найден")
+            //showErrorAlert(alertText: "Ошибка", alertMessage: "Пользователь не найден")
+            animateWrongPassword()
             return false
         }
     }
